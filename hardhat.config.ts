88977b67227bbc27d9da67d81a87ddcf8053a3c8
Shayan <shayanyeg25@gmail.com>
2026-08-05 19:49:25 +0330

    Deploy project on Arc Testnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
 import { configVariable, defineConfig } from "hardhat/config";
 
@@ -33,6 +34,12 @@ export default defineConfig({
       chainType: "l1",
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+        arcTestnet: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("ARC_RPC_URL"),
+      accounts: [configVariable("ARC_PRIVATE_KEY")],
     },
   },
 });
